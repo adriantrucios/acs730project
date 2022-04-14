0816490b0de469200c6a91206142fcbfb5d7e895
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2022-04-14 17:08:01 -0400

    added modules/aws_network_staging/output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,12 @@
-
-output "public_subnet_ids_staging" {
-  value = module.vpc-staging.public_subnet_ids
+# Add output variables
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet[*].id
 }
 
-output "private_subnet_ids_staging" {
-  value = module.vpc-staging.private_subnet_ids
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
 }
 
-output "vpc_id_staging" {
-  value = module.vpc-staging.vpc_id
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
-
-

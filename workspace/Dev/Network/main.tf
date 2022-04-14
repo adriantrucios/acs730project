991e1f940d84bde9008e0de9eb6bcddc0c724a8c
gsharma0610 <gsharma96@myseneca.ca>
2022-04-14 08:25:11 +0000

    network implementation

---
+++ workspace/Dev/Network/main.tf
@@ -0,0 +1,8 @@
+module "dev" {
+  source              = "../../../modules/Network"
+  env                 = var.env
+  vpc_cidr            = var.vpc_cidr
+  private_cidr_blocks = var.private_cidr_blocks
+  public_cidr_blocks  = var.public_cidr_blocks
+  prefix              = var.prefix
+}

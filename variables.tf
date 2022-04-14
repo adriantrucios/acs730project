7355977b56a78e489d3f44ab503b9d6b27ce8d80
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2022-04-14 17:10:05 -0400

    added module/aws_network_staging/variables.tf

--- variables.tf
+++ variables.tf
@@ -1,37 +1,32 @@
 # Default tags
 variable "default_tags" {
   default = {
-    "Owner" = "Aparna",
-    "App"   = "Web"
+    "staging" = "t3.small"
   }
-  type        = map(any)
+  type        = map(string)
   description = "Default tags to be appliad to all AWS resources"
 }
 
 # Name prefix
 variable "prefix" {
   type        = string
-  default     = "group10"
   description = "Name prefix"
 }
 
 # Provision public subnets in custom VPC
-variable "public_subnet_cidrs" {
-  default     = ["10.200.1.0/24", "10.200.2.0/24", "10.200.3.0/24"]
+variable "public_cidr_blocks" {
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 
 # Provision private subnets in custom VPC
-variable "private_subnet_cidrs" {
-  default     = ["10.200.4.0/24", "10.200.5.0/24", "10.200.6.0/24"]
+variable "private_cidr_blocks" {
   type        = list(string)
   description = "Private Subnet CIDRs"
 }
 
 # VPC CIDR range
 variable "vpc_cidr" {
-  default     = "10.200.0.0/16"
   type        = string
   description = "VPC to host static web site"
 }
@@ -42,3 +37,4 @@ variable "env" {
   type        = string
   description = "Deployment Environment"
 }
+

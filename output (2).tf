ff638e1eb81d5218a737b9d7d4c1e0f1ff921590
apalem1205 <99748602+apalem1205@users.noreply.github.com>
2022-04-14 16:22:00 -0400

    removed

--- output (2).tf
+++ output (2).tf
@@ -1,12 +1,1 @@
-# Default tags
-output "default_tags" {
-  value = {
-    "Owner" = "Aparna"
-    "App"   = "Web"
-  }
-}
 
-# Prefix to identify resources
-output "prefix" {
-  value     = "group10"
-}

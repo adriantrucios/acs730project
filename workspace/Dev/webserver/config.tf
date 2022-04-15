041271c394392396f8a93fc8728f15bdf6960f3e
rvaniya <rvaniya@myseneca.ca>
2022-04-15 20:34:17 +0000

    Edited Static Webserver

--- workspace/Dev/webserver/config.tf
+++ workspace/Dev/webserver/config.tf
@@ -1,7 +1,7 @@
 # S3 Bucket Dev webserver
 terraform {
   backend "s3" {
-    bucket = "group-10-acs-project"
+    bucket = "group-10-acs-project-dev"
     key    = "dev/webserver/terraform.tfstate"
     region = "us-east-1"
   }
--- workspace/prod/webserver/config.tf
+++ workspace/prod/webserver/config.tf
@@ -1,7 +1,7 @@
 # S3 Bucket Dev webserver
 terraform {
   backend "s3" {
-    bucket = "group-10-acs-project"
+    bucket = "group-10-acs-project-prod"
     key    = "prod/webserver/terraform.tfstate"
     region = "us-east-1"
   }

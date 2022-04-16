88dc94eed624ccc3cee620cda563bff5bb6c1036
adriantrucios <102596858+adriantrucios@users.noreply.github.com>
2022-04-16 18:38:38 -0400

    updating S3 bucket name

--- workspace/Staging/Network/config.tf
+++ workspace/Staging/Network/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "group-10-acs-project"
+    bucket = "acs730project-staging"
     key    = "staging/network/terraform.tfstate"
     region = "us-east-1"
   }
